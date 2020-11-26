4581866ffb390d9758d33efb7ea1117d35ae5342
lyj1998 <3301056761@qq.com>
2020-11-26 14:10:28 +0800

    sort

--- src/algorithm/test/TestComparable.java
+++ src/algorithm/test/TestComparable.java
@@ -1,7 +1,5 @@
 package algorithm.test;
-
 import algorithm.sort.Student;
-
 //2.定义测试类Test，在测试类Test定义测试方法Comparable getMax(Comparable c1, Comparable c2)完成测试
 public class TestComparable {
     public static void main(String[] args){
@@ -11,10 +9,10 @@ public class TestComparable {
         s1.setAge(18);
 
         Student s2 = new Student();
-        s1.setUsername("张三");
-        s1.setAge(18);
-
-
+        s2.setUsername("李四");
+        s2.setAge(20);
+        Comparable max = getMax(s1, s2);
+        System.out.println(max);
     }
     public static Comparable getMax(Comparable c1, Comparable c2){
         int result = c1.compareTo(c2);
